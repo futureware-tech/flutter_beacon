845fdd100e77f912080f1a05eec61528e16e6fab
Katarina Sheremet <kate.romanowskaya@gmail.com>
2022-05-05 23:09:08 +0200

    Migrate Android project to embedding 2
    https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects

--- example/android/app/src/main/java/com/flutterbeaconexample/MainActivity.java
+++ example/android/app/src/main/java/com/flutterbeaconexample/MainActivity.java
@@ -1,16 +1,9 @@
 package com.flutterbeaconexample;
 
-import android.os.Bundle;
 
-import com.flutterbeacon.FlutterBeaconPlugin;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
 }
